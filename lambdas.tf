f7fb59e214e761774ea6a19e14c890937ea73c1c
Alon Almog <alon.almog@gmail.com>
2023-06-01 08:12:55 +0300

    forgot to save lambdas.tf

--- lambdas.tf
+++ lambdas.tf
@@ -1,56 +1,50 @@
-resource "aws_lambda_function" "calculate_occurrences" {
-  filename         = "ex_lambda_calc_occr.zip"
-  function_name    = "ex_lambda_calc_occr"
-  role             = aws_iam_role.lambda_role.arn
-  handler          = "ex_lambda_calc_occr.lambda_handler"
-  runtime          = "python3.8"
-  timeout          = 10
-  memory_size      = 128
+provider "archive" {}
 
-  source_code_hash = filebase64sha256("ex_lambda_calc_occr.zip")
+data "archive_file" "zip_occr" {
+  type        = "zip"
+  source_file = "ex_lambda_calc_occr.py"
+  output_path = "ex_lambda_calc_occr.zip"
 }
 
-resource "aws_iam_role" "lambda_role" {
-  name = "lambda_role"
+data "archive_file" "zip_read" {
+  type        = "zip"
+  source_file = "ex_lambda_readbyid.py"
+  output_path = "ex_lambda_readbyid.zip"
+}
 
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "",
-      "Effect": "Allow",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Action": "sts:AssumeRole"
+data "aws_iam_policy_document" "policy" {
+  statement {
+    sid    = ""
+    effect = "Allow"
+
+    principals {
+      identifiers = ["lambda.amazonaws.com"]
+      type        = "Service"
     }
-  ]
-}
-EOF
+
+    actions = ["sts:AssumeRole"]
+  }
 }
 
-resource "aws_iam_role_policy_attachment" "lambda_policy_attachment" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+resource "aws_iam_role" "iam_for_lambda" {
+  name               = "iam_for_lambda"
+  assume_role_policy = "${data.aws_iam_policy_document.policy.json}"
+}
+
+resource "aws_lambda_function" "calculate_occurrences" {
+  function_name    = "ex_lambda_calc_occr"
+  filename         = "${data.archive_file.zip_occr.output_path}"
+  source_code_hash = "${data.archive_file.zip_occr.output_base64sha256}"
+  role             = "${aws_iam_role.iam_for_lambda.arn}"
+  handler          = "ex_lambda_calc_occr.lambda_handler"
+  runtime          = "python3.9"
 }
 
 resource "aws_lambda_function" "retrieve_result" {
-  filename         = "ex_lambda_readbyid.zip"
   function_name    = "ex_lambda_readbyid"
-  role             = aws_iam_role.lambda_role.arn
+  filename         = "${data.archive_file.zip_read.output_path}"
+  source_code_hash = "${data.archive_file.zip_read.output_base64sha256}"
+  role             = "${aws_iam_role.iam_for_lambda.arn}"
   handler          = "ex_lambda_readbyid.lambda_handler"
-  runtime          = "python3.8"
-  timeout          = 10
-  memory_size      = 128
-
-  source_code_hash = filebase64sha256("ex_lambda_readbyid.zip")
-}
-
-output "calculate_occurrences_lambda_arn" {
-  value = aws_lambda_function.calculate_occurrences.arn
-}
-
-output "retrieve_result_lambda_arn" {
-  value = aws_lambda_function.retrieve_result.arn
+  runtime          = "python3.9"
 }
